f6549a7122ea4c2d5a21ccd93e4e347e7fea1968
plrabbit <srabbit38@163.com>
2020-02-24 17:34:27 +0800

    :art: Initial loading spin

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,29 @@
   </div>
 </template>
 
+<script>
+  /* Remove the style element in public/index.html */
+  const removeIndexStyle = function() {
+    const _element = window.document.getElementById('initial-loading-spin')
+    const _parentElement = _element.parentNode;
+    if(_parentElement){
+      _parentElement.removeChild(_element);
+    }
+  }
+
+  export default {
+    beforeCreate() {
+      removeIndexStyle()
+    }
+  }
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
