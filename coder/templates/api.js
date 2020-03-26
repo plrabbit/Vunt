8f523d56e4a7bf69c67c540234472bd1b4d6032c
plrabbit <srabbit38@163.com>
2020-03-26 17:59:27 +0800

    refactoring templates

--- coder/templates/api.js
+++ coder/templates/api.js
@@ -7,8 +7,10 @@ import axios from '@/helper/axios'
 import { <%= PREFIX_HOST_NAME %> } from '@/config'
 
 <% const funcNameList = [] %>
+<% let hasRest = false %>
 <% _.each(apiList, function (api) { %>
   <% funcNameList.push(api.name) %>
+  <% if (api.rest) hasRest = true %>
   /** <%= api.desc %> */
   <% const { pathParams, arrangedPath } = getPathParams(api.path) %>
 
@@ -17,12 +19,14 @@ import { <%= PREFIX_HOST_NAME %> } from '@/config'
   <% const hasPathParams = pathParams.length > 0 %>
   export const <%= api.name %> = function (<%if (api.rest) { %>method, <% } %><%if (hasPathParams) { %>pathParams = {}, <% } %>data = {}) {
     <%if (hasPathParams) { %>const { <%= pathParams.join(', ') %> } = pathParams<% } %>
-    return axios({
+    const config = {
       <% const method = api.options.method.toLowerCase() %>
-      <% if ( method === 'get' || method === 'delete' ) { %>
-        params: data,
-      <% } else { %>
-        data,
+      <% if (!api.rest) { %>
+        <% if (method === 'get' || method === 'delete') { %>
+          params: data,
+        <% } else { %>
+          data,
+        <% } %>
       <% } %>
       <%if(api.options){%>
         <% for (let [key, value] of Object.entries(api.options)) { %>
@@ -37,11 +41,32 @@ import { <%= PREFIX_HOST_NAME %> } from '@/config'
         <% } %>
       <% } %>
       url: <%= PREFIX_HOST_NAME %> + <% if (pathParams.length) { %>\`<%= arrangedPath %>\`<% } else { %>'<%= arrangedPath %>'<% } %>
-    })
+    }
+    <% if (api.rest) { %>
+    injectData(config, data)
+    <% } %>
+    return axios(config)
   }
 <% }) %>
 
 <% handleDuplicatedFunctionName(funcNameList) %>
+
+<% if (hasRest) { %>
+/** (API)Inject Data in config */
+const injectData = function (config, data) {
+  if (typeof method !== 'string') return false
+  if (/^(get|post|put|patch|delete|head|options)$/i.test(config.method)) {
+    if (/^(get|delete)$/i.test(config.method)) {
+      config = Object.assign({}, config, { params: data })
+    } else {
+      config = Object.assign({}, config, { data })
+    }
+    return config
+  } else {
+    return false
+  }
+}
+<% } %>
 `, {
   imports: utils
 })
