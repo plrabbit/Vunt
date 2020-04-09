71cd275239dd2e4ce01f8aa12b0318012dc56a9f
plrabbit <srabbit38@163.com>
2020-04-09 18:16:47 +0800

    Code cleaning

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,26 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <a-button @click="handleClick" type="primary">Click me!</a-button>
-    <a-button @click="handleCancel" type="danger">Cancel!</a-button>
+    <a-button type="primary">Click me!</a-button>
   </div>
 </template>
-
-<script>
-import baseFeatures from '@/base/mixin/base-features'
-
-export default {
-  mixins: [baseFeatures],
-  methods: {
-    handleClick () {
-      this.$api.getArticles('GET', {
-        m: 'article',
-        a: 'list'
-      })
-    },
-    handleCancel () {
-      this.$cancelRequest('GET', this.$api.getArticles)
-    }
-  }
-}
-</script>
