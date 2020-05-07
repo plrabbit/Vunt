04e266df2890729ff401f44ab862c2a84b5f7338
plrabbit <srabbit38@163.com>
2020-05-07 10:42:37 +0800

    Optimize routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,14 @@
-const _import = file => import('@/views/' + file + '.vue')
+const _import = require('@/utils/view-import/' + process.env.NODE_ENV)
 
-module.exports = [
+export default [
   {
     path: '/',
     name: 'Home',
-    component: () => _import('Home')
+    component: _import('Home')
   },
   {
     path: '/about',
     name: 'About',
-    component: () => _import('About')
+    component: _import('About')
   }
 ]
