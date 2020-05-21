edbc9e1379c517ccda74ad5daf0ec139472464c6
plrabbit <srabbit38@163.com>
2020-05-21 13:53:28 +0800

    device.js

--- src/utils/device.js
+++ src/utils/device.js
@@ -1,4 +1,5 @@
 import enquireJs from 'enquire.js'
+import { mapState } from 'vuex'
 
 export const DEVICE_TYPE = {
   DESKTOP: 'desktop',
@@ -13,7 +14,7 @@ export const deviceEnquire = function (callback) {
     }
   }
 
-  const matchLablet = {
+  const matchTablet = {
     match: () => {
       callback && callback(DEVICE_TYPE.TABLET)
     }
@@ -28,6 +29,45 @@ export const deviceEnquire = function (callback) {
   // screen and (max-width: 1087.99px)
   enquireJs
     .register('screen and (max-width: 576px)', matchMobile)
-    .register('screen and (min-width: 576px) and (max-width: 1199px)', matchLablet)
+    .register('screen and (min-width: 576px) and (max-width: 1199px)', matchTablet)
     .register('screen and (min-width: 1200px)', matchDesktop)
+}
+
+export const mixinDevice = {
+  computed: {
+    ...mapState({
+      device: state => state.app.device
+    })
+  },
+  methods: {
+    isMobile () {
+      return this.device === DEVICE_TYPE.MOBILE
+    },
+    isDesktop () {
+      return this.device === DEVICE_TYPE.DESKTOP
+    },
+    isTablet () {
+      return this.device === DEVICE_TYPE.TABLET
+    }
+  }
+}
+
+export const AppDeviceEnquire = {
+  mounted () {
+    const { $store } = this
+    deviceEnquire(deviceType => {
+      switch (deviceType) {
+        case DEVICE_TYPE.DESKTOP:
+          $store.commit('TOGGLE_DEVICE', 'desktop')
+          break
+        case DEVICE_TYPE.TABLET:
+          $store.commit('TOGGLE_DEVICE', 'tablet')
+          break
+        case DEVICE_TYPE.MOBILE:
+        default:
+          $store.commit('TOGGLE_DEVICE', 'mobile')
+          break
+      }
+    })
+  }
 }
