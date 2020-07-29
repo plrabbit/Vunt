b45603b074de5afd094d62028d86f57fa36574a5
plrabbit <srabbit38@163.com>
2020-07-29 17:08:40 +0800

    Get current browser

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,3 +9,54 @@ export const guid = function () {
     return v.toString(16)
   })
 }
+
+/**
+ * Get current browser
+ * @returns {string} Browser name
+ */
+export const getCurrentBrowser = function () {
+  const userAgent = navigator.userAgent
+  const isOpera = userAgent.indexOf('Opera') > -1
+  const isIE = userAgent.indexOf('compatible') > -1 &&
+    userAgent.indexOf('MSIE') > -1 && !isOpera
+  const isEdge = userAgent.indexOf('Edge') > -1
+  const isFF = userAgent.indexOf('Firefox') > -1
+  const isSafari = userAgent.indexOf('Safari') > -1 &&
+    userAgent.indexOf('Chrome') === -1
+  const isChrome = userAgent.indexOf('Chrome') > -1 &&
+    userAgent.indexOf('Safari') > -1
+
+  if (isIE) {
+    const reIE = new RegExp('MSIE (\\d+\\.\\d+);')
+    reIE.test(userAgent)
+    const fIEVersion = parseFloat(RegExp.$1)
+    if (fIEVersion === 7) {
+      return 'IE7'
+    } else if (fIEVersion === 8) {
+      return 'IE8'
+    } else if (fIEVersion === 9) {
+      return 'IE9'
+    } else if (fIEVersion === 10) {
+      return 'IE10'
+    } else if (fIEVersion === 11) {
+      return 'IE11'
+    } else {
+      return '0'
+    }
+  }
+  if (isOpera) {
+    return 'Opera'
+  }
+  if (isEdge) {
+    return 'Edge'
+  }
+  if (isFF) {
+    return 'FF'
+  }
+  if (isSafari) {
+    return 'Safari'
+  }
+  if (isChrome) {
+    return 'Chrome'
+  }
+}
