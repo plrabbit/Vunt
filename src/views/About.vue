01ba23612a64ddffaf11d767d350c2ae49af4a87
plrabbit <srabbit38@163.com>
2020-09-02 18:03:53 +0800

    The outer frame

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,21 +2,37 @@
   <div class="about">
     <h1>This is an about page</h1>
     <a-timer-picker/>
+    <div style="margin-top: 24px;">
+      <icon-picker v-model="currentIcon" />
+    </div>
   </div>
 </template>
 
 <script>
 import { TimePicker } from 'ant-design-vue'
+import IconPicker from '@/widgets/IconPicker/IconPicker'
 
 export default {
   components: {
+    IconPicker,
     ATimerPicker: TimePicker
+  },
+  data () {
+    return {
+      currentIcon: 'deployment-unit'
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
+@import "../style/variables";
+
 .about {
   margin: 24px 0 0 0;
+}
+
+.iconPicker {
+  font-size: 48px;
 }
 </style>
---
+++ src/widgets/IconPicker/IconPicker.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="iconPicker iconPickerBtn">
+    <a-icon :type="value" />
+    <div class="containerChoosing">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Icon } from 'ant-design-vue'
+
+Vue.use(Icon)
+
+export default {
+  name: 'IconPicker',
+  model: {
+    event: 'change'
+  },
+  props: {
+    value: {
+      type: String,
+      default: () => ''
+    }
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.iconPickerBtn {
+  padding: 6px;
+  line-height: 1;
+  border: 1px #cccccc solid;
+  border-radius: .1em;
+  display: inline-block;
+  cursor: pointer;
+
+  > .anticon {
+    vertical-align: bottom;
+  }
+}
+</style>
